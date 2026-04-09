5f5436e6b5201b05d8c8d332f3b54733ed615a3c
diagutierrezro <diagutierrezro@unal.edu.co>
2026-04-09 12:57:30 -0500

    fix: Cambio por ajuste en la db

--- models/soporte_solicitud.go
+++ models/soporte_solicitud.go
@@ -10,9 +10,9 @@ import (
 )
 
 type SoporteSolicitud struct {
-	Id          int `orm:"column(id);pk;auto"`
-	DocumentoId int `orm:"column(documento_id)"`
-	//TipoDocumentoId          *int                    `orm:"column(tipo_documento_id);null"`
+	Id                       int                     `orm:"column(id);pk;auto"`
+	DocumentoId              int                     `orm:"column(documento_id)"`
+	TipoDocumentoId          *int                    `orm:"column(tipo_documento_id);null"`
 	TerceroId                int                     `orm:"column(tercero_id)"`
 	Activo                   bool                    `orm:"column(activo)"`
 	FechaCreacion            string                  `orm:"column(fecha_creacion);type(timestamp without time zone)"`
